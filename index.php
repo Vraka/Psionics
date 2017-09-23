216de641b33ab97c839ec0806bbc93636f1946af
Vraka <nrajan30@gatech.edu>
2017-09-22 22:39:12 -0400

    First push"

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php 
+
+?>
+
+<head>
+<title> The Codex </title>
+</head>
+
+<body>
+<center><h1> The Codex </h1></center>
+<center><h2><a href="kin/kitsune.php">Enter</a></h2></center>
+</body>
---
+++ kin/kitsune.php
@@ -0,0 +1,23 @@
+<head>
+<title>Codex: Kitsune</title>
+</head>
+
+<body>
+<h1>Kitsune</h1>
+<h2>Overview</h2>
+<h4><p>The kitsune are a race of fox yokai of Shinto origin. Created by the fox goddess Inari, the kitsune are mischievous and ellusive. While they love to play tricks on others, they hide away their villages away from prying eyes. Kitsune are rarely seen in their true form, if at all.</p></h4>
+<br>
+<h2>Appearance</h2>
+<h4><p>As polymorphic beings, the kitsune can take on virtually any form they choose. However, their base form is that of a multi-tailed fox, with lighter fur colors. They are ususally thin and lean, as kitsune are primarily speed fighters as opposed to brute strength. In fact, their tails are the strongest limbs and can be used to attack, move, or pick up objects.</p></h4>
+<br>
+<h2>Abilities</h2>
+<h4><p>Kitsune can have anywhere between one and nine tails, each tail representing a level of mastery over one of their nine abilities: Shapeshifting, Fox Fire, Pocket Realm (PR) Creation, Illusion, Main Affinity Control, 4 Basic Element Control, Seduction, Possession, and Dreamwalking. While the first tail a kitsune is born with is usually Shapeshifting, the order in which the subsequent tails are earned depends on which ability the kitsune masters next.</p></h4>
+<h3><p>Shapeshifting</p></h3>
+<h4><p>Generally the first ability a kitsune has when they are born, kitsune are able to polymorph into virtually anything. In order to achieve more complex shifts, for example gills to filter oxygen out of water, kitsune need to have a clear idea of not only how their desired outcome looks, but also how it functions on a base level. Additionally, while simple shifts take negligible energy to hold indefinitely, complex shifting and especially large changes in mass require more energy, and are much harder to maintain.</p></h4>
+<h3><p>Fox Fire</p></h3>
+<h4><p>Despite the name, Fox Fire isn't necessarily a fire based ability. Rather it's flame like constructs made from</p></h4>
+<h3><p>The Orb</p></h3>
+<h4><p>A few kitsune have an orb that contains the majority of their power. While</p></h4>
+<h2>Related Entries</h2>
+Inari, Nogitsune
+</body>
